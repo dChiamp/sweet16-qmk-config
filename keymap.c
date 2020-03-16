b909ba97ec814bafd0dcede78d20660d13164c7a
hephaestus <hephaestus@hephaestuss-MBP.lan>
2020-03-15 23:57:38 -0700

    reorganizes layers

--- keymap.c
+++ keymap.c
@@ -2,7 +2,7 @@
 
 #define _DEFAULT 0
 #define _CMNDS 1
-// #define _MEDIA 2
+#define _MEDIA 2
 
 enum custom_keycodes {
   UP_URL = SAFE_RANGE,
@@ -10,35 +10,38 @@ enum custom_keycodes {
   SCRNSHOT,
   RUNDEV,
   COMMIT,
+  PUSH,
   FU,
   COMMENT,
   CUT,
   UNDO,
-  SLCTALL
+  SLCTALL,
+  COPY,
+  PASTE
 
 };
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [_DEFAULT] = LAYOUT_ortho_4x4(
-        KC_7,   KC_8,   KC_9,   KC_ASTR,
-        KC_4,   KC_5,   KC_6,   KC_SLSH,
-        KC_1,   KC_2,   KC_3,   KC_MINS,
+        KC_7,   KC_8,   KC_9,    KC_ASTR,
+        KC_4,   KC_5,   KC_6,    KC_SLSH,
+        KC_1,   KC_2,   KC_3,    MO(_CMNDS),
         KC_ENT, KC_0,   KC_BSPC, TO(1)
     ),
 
-    [_CMNDS] = LAYOUT_ortho_4x4(
-       KC_MUTE,   KC_VOLD,  KC_VOLU,   KC_MPLY,
-       KC_MRWD,   KC_MFFD,  FU,        SCRNSHOT,
-       CMDCLEAR,  RUNDEV,   COMMIT,    COMMENT,
-       SLCTALL,   CUT,      UNDO,     TO(0)
-    )
+    [_MEDIA] = LAYOUT_ortho_4x4(
+       KC_MUTE,   KC_VOLD,    KC_VOLU,   KC_MPLY,
+       KC_MRWD,   KC_MFFD,    COMMENT,   CMDCLEAR,
+       SCRNSHOT,  KC_UP,      COMMIT,    FU,
+       KC_LEFT,   KC_DOWN,    KC_RIGHT,   TO(0)
+    ),
 
-    // [_MEDIA] = LAYOUT_ortho_4x4(
-    //    KC_MUTE,   KC_VOLD,  KC_VOLU,   KC_MPLY,
-    //    KC_MRWD,   KC_MFFD,  COMMENT,   CMDCLEAR,
-    //    SCRNSHOT,  RUNDEV,   COMMIT,    FU,
-    //    KC_ENT,    KC_CUT,   KC_BSPC,   TO(0)
-    // )
+    [_CMNDS] = LAYOUT_ortho_4x4(
+       SLCTALL,   COPY,     PASTE,     UNDO,
+       CMDCLEAR,  CUT,      FU,        SCRNSHOT,
+       RUNDEV,    COMMIT,   PUSH,      COMMENT,
+       KC_ENT,    KC_NO,    KC_BSPC,   KC_NO
+    )
 
 };
 
@@ -65,6 +68,12 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
           case COMMIT:
               if(record->event.pressed){
                   SEND_STRING("git commit -am ");
+              }
+              return false;
+              break;
+          case PUSH:
+              if(record->event.pressed){
+                  SEND_STRING("git push origin ");
               }
               return false;
               break;
@@ -115,6 +124,22 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
               }
               return false;
               break;
+          case COPY:
+            if (record->event.pressed) {
+                register_code(KC_LGUI);
+                tap_code(KC_C);
+                unregister_code(KC_LGUI);
+            }
+            return false;
+            break;
+          case PASTE:
+            if (record->event.pressed) {
+                register_code(KC_LGUI);
+                tap_code(KC_V);
+                unregister_code(KC_LGUI);
+            }
+            return false;
+            break;
       }
 
     }
